62b83b351a14ca5afcbf11e51ece35195cf88e26
darpan <darpansingh8686@gmail.com>
2020-07-03 10:52:40 +0530

    Django-pr

--- mywebproject/settings.py
+++ mywebproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mywebproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/kolkata'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
