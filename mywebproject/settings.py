94fc8c57251e98753db9657fad015e85048b3767
darpan <darpansingh8686@gmail.com>
2020-07-11 11:04:14 +0530

    submit

--- mywebproject/settings.py
+++ mywebproject/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER='darpandev010107@gmail.com'
+EMAIL_HOST_USER='ddev010107@gmail.com'
 EMAIL_HOST_PASSWORD='developer01d'
 EMAIL_USE_TLS=True
 
